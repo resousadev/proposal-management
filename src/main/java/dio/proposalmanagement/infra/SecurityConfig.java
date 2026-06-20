f59411c1bb18185dd44e4a51aff27014b2497911
resousadev <resousadev@gmail.com>
2026-06-20 20:03:19 -0300

    feat: Adicionar UserDetailsService com usuário em memória - Criar bean UserDetailsService com InMemoryUserDetailsManager - Adicionar usuário 'influencer' com senha 'password' - Configurar role 'INFLUENCER' para o usuário - Utilizar senha codificada com withDefaultPasswordEncoder() - Permitir autenticação de usuários locais na aplicação

--- src/main/java/dio/proposalmanagement/infra/SecurityConfig.java
+++ src/main/java/dio/proposalmanagement/infra/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,6 +24,17 @@ public class SecurityConfig {
       .formLogin(Customizer.withDefaults());
 
     return http.build();
+  }
+
+  @Bean
+  UserDetailsService userDetailsService() {
+    UserDetails user = User.withDefaultPasswordEncoder()
+      .username("influencer")
+      .password("password")
+      .roles("INFLUENCER")
+      .build();
+
+    return new InMemoryUserDetailsManager(user);
   }
 
 }
